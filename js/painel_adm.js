2bcf7aa2c159fc00a2459ef616e41a67ee74ce36
GiulliaGentil <giulliafernandes423@gmail.com>
2023-08-23 16:47:59 -0300

    atualizado

---
+++ js/painel_adm.js
@@ -0,0 +1,13 @@
+const lista = document.getElementById( 'lista');
+
+//array da nossa lista 
+const listaDeClientes =['Kaian', 'Julia', 'Lucas'];
+
+//função que exibirá nossa lista
+const exibirLista = () => {
+    for(let i =0; i < listaDeClientes.length; i++){
+        lista.innerHTML += listaDeClientes[i];
+    }
+}
+
+exibirLista()
